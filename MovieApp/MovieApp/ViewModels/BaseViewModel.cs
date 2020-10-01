fbe26692f785c8465057f542d3b1ab7340a963fe
Jesus Vara <jesus.vara@softtek.com>
2020-10-01 16:13:16 -0500

    - se crea baseviewodel, se crea mainpageviewmodel

---
+++ MovieApp/MovieApp/ViewModels/BaseViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace MovieApp.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public BaseViewModel()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private bool isLoading;
+        public bool IsLoading
+        {
+            get { return isLoading; }
+            set
+            { isLoading = value; OnPropertyChanged("IsLoading"); }
+        }
+
+        private bool isBusy;
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set
+            { isBusy = value; OnPropertyChanged("IsBusy"); }
+        }
+
+    }
+}
---
+++ MovieApp/MovieApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using MovieApp.Models;
+
+namespace MovieApp.ViewModels
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        private Root root;
+
+        private List<Result> topRate;
+        private List<Result> upcoming;
+        private List<Result> popular;
+
+
+        public List<Result> TopRate
+        {
+            get { return topRate; }
+            set
+            { topRate = value; OnPropertyChanged("TopRate"); }
+        }
+
+        public List<Result> Upcoming
+        {
+            get { return upcoming; }
+            set
+            { upcoming = value; OnPropertyChanged("Upcoming"); }
+        }
+
+        public List<Result> Popular
+        {
+            get { return popular; }
+            set
+            { popular = value; OnPropertyChanged("Popular"); }
+        }
+
+
+        public MainPageViewModel()
+        {
+            root = new Root();
+            TopRate = new List<Result>();
+            Upcoming = new List<Result>();
+            Popular = new List<Result>();
+            GetBannersAsync();
+        }
+
+        public async void GetBannersAsync()
+        {
+            IsLoading = true;
+            TopRate = await root.GetTopRate();
+            Upcoming = await root.GetUpconming();
+            Popular = await root.GetPopular();
+            IsLoading = false;
+        }
+
+
+
+
+    }
+}
