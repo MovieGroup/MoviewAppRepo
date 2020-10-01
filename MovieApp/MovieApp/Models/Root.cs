58be0461373ab064da9be2b67dba7d1b85894dc9
Jesus Vara <jesus.vara@softtek.com>
2020-10-01 16:12:48 -0500

    - se modifica la vista del home - se crean los metodos de obtencion de datos del servicio para las peliculas

--- MovieApp/MovieApp/Models/Root.cs
+++ MovieApp/MovieApp/Models/Root.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
@@ -18,31 +19,76 @@ namespace MovieApp.Models
 
         //CONSTANTES
         private string api_key = "a53f89eb22343648161f97add0de0bbf";
-        private string topRateUri = "https://api.themoviedb.org/3/movie/top_rated?";
-        private string upconmingUri = "https://api.themoviedb.org/3/movie/upcoming";
-        private string popularUri = "https://api.themoviedb.org/3/movie/popular";
+        private string topRateUri = "https://api.themoviedb.org/3/movie/top_rated?api_key=";
+        private string upconmingUri = "https://api.themoviedb.org/3/movie/upcoming?api_key=";
+        private string popularUri = "https://api.themoviedb.org/3/movie/popular?api_key=";
         private string endUri = "&language=en-US&page=1";
+        private string imageUrl = "http://image.tmdb.org/t/p/w500/";
 
 
 
 
         //METODOS
-        public async Task<Root> GetTopRate()
+        public async Task<List<Result>> GetTopRate()
         {
-            Root root = new Root();
-            return root;
+            Root resp;
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(string.Concat(topRateUri, api_key, endUri));
+                var json = await response.Content.ReadAsStringAsync();
+                resp = JsonConvert.DeserializeObject<Root>(json);
+            }
+            return filterMethod(resp);
         }
 
-        public async Task<Root> GetUpcoming()
+        public async Task<List<Result>> GetUpconming()
         {
-            Root root = new Root();
-            return root;
+            Root resp;
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(string.Concat(upconmingUri, api_key, endUri));
+                var json = await response.Content.ReadAsStringAsync();
+                resp = JsonConvert.DeserializeObject<Root>(json);
+            }
+            return filterMethod(resp);
         }
 
-        public async Task<Root> GetPopular()
+        public async Task<List<Result>> GetPopular()
         {
-            Root root = new Root();
-            return root;
+            Root resp;
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(string.Concat(popularUri, api_key, endUri));
+                var json = await response.Content.ReadAsStringAsync();
+                resp = JsonConvert.DeserializeObject<Root>(json);
+            }
+            return filterMethod(resp);
+        }
+
+
+        private List<Result> filterMethod(Root data)
+        {
+            List<Result> result = new List<Result>();
+
+            if (data.results.Count >= 10)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    data.results[i].poster_path = string.Concat(imageUrl + data.results[i].poster_path);
+                    data.results[i].backdrop_path = string.Concat(imageUrl + data.results[i].backdrop_path);
+                    result.Add(data.results[i]);
+                }
+            }
+            else
+            {
+                for (int i = 0; i <= data.results.Count; i++)
+                {
+                    data.results[i].poster_path = string.Concat(imageUrl + data.results[i].poster_path);
+                    data.results[i].backdrop_path = string.Concat(imageUrl + data.results[i].backdrop_path);
+                    result.Add(data.results[i]);
+                }
+            }
+            return result;
         }
 
     }
--- MovieApp/MovieApp/Views/MainPage.xaml.cs
+++ MovieApp/MovieApp/Views/MainPage.xaml.cs
@@ -1,15 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-
+﻿
+using MovieApp.ViewModels;
 using Xamarin.Forms;
 
 namespace MovieApp.Views
 {
     public partial class MainPage : ContentPage
     {
+        MainPageViewModel VM;
         public MainPage()
         {
             InitializeComponent();
+            VM = new MainPageViewModel();
+            this.BindingContext = VM;
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+            //Root root = new Root();
+            //List<Result> a = await root.GetTopRate();
+            //List<Result> b = await root.GetUpconming();
+            //List<Result> c = await root.GetPopular();
+            //string k = "";
+            // await Navigation.PushModalAsync(new DetailPage());
+
         }
     }
 }
